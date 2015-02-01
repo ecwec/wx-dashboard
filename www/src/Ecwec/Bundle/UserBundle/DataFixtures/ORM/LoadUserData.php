46202441337ead36dcc0529ca6300905ffdee5bf
Ben Benesh <benesh9@gmail.com>
2015-01-31 20:12:33 -0600

    Adds testing/dev data.

---
+++ www/src/Ecwec/Bundle/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Ecwec\Bundle\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Ecwec\Bundle\UserBundle\Entity\User;
+
+class LoadUserData implements FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        // Create our user and set details
+        /** @var User $user */
+        $user = $userManager->createUser();
+        $user->setUsername('admin');
+        $user->setEmail('admin@ecwec.org');
+        $user->setPlainPassword('admin');
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_ADMIN'));
+
+        // Update the user
+        $userManager->updateUser($user, true);
+    }
+}
+ 
