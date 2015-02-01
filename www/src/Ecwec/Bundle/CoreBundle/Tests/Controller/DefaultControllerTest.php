c4e7641f7a142c987288710eb1e16c8a9aa3f5f5
Ben Benesh <benesh9@gmail.com>
2015-01-31 20:19:58 -0600

    Removes example integration test.

--- www/src/Ecwec/Bundle/CoreBundle/Tests/Controller/DefaultControllerTest.php
+++ www/src/Ecwec/Bundle/CoreBundle/Tests/Controller/DefaultControllerTest.php
@@ -8,10 +8,7 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
+        //@TODO: Add integration test.
         $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
     }
 }
